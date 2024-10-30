50da50e48aa51836c466ba4165be5199aa43fddd
JeffreyTsai <jeffreytsai1004@gmail.com>
2024-10-31 04:44:53 +0800

    Update

--- Compile.bat
+++ Compile.bat
@@ -1,16 +1,9 @@
 ﻿@echo off
-chcp 65001 > nul
 
 :: 激活虚拟环境
-call .\venv\activate.bat
-<nul set /p =虚拟环境已激活
-echo.
+call "%VIRTUAL_ENV%\Scripts\activate.bat"
 
-:: 使用PyInstaller编译
-<nul set /p =正在编译...
-echo.
-python -m PyInstaller decompiler.spec --clean
+:: 编译
+"%VIRTUAL_ENV%\python.exe" -m pyinstaller decompiler.spec --clean
 
-<nul set /p =编译完成！
-echo.
-pause
+
